51736bbf6deac8c5580d7cf8cf7b3affe8acca7f
Eugene Kvasnyi <e.kvasnyi@gmail.com>
2021-05-14 18:24:20 +0300

    Added: inRange method

--- _.js
+++ _.js
@@ -10,6 +10,35 @@ const _ = {
         //alt implementation
         const maxNumber = Math.max(number, lower);
         return Math.min(maxNumber, upper);
+    },
+    /**
+     * checks to see if the provided number falls 
+     * within the range specified by the start and end values
+     */
+    inRange(number, start, end){
+        // If no end value is provided to the method, the start value will be 0 and the end value will be the provided start value
+        if(end === undefined){
+            // end = start;
+            // start = 0;
+            [end, start] = [start, 0]
+        }
+
+
+        // If the provided start value is larger than the provided end value, the two values should be swapped
+        if(start>end){
+            [start,end] = [end,start]
+        }
+
+        // If the provided number is within the start and end values, .inRange() will return true
+        if(number>=start && number < end){
+            return true;
+        }
+
+        // If the provided number is smaller than the start value, .inRange() will return false
+        // If the provided number is larger than or equal to the end value, .inRange() will return false
+        return false;
+
+
     }
 }
 
