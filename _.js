9a3319d1fda1d6d1d6f4528561f759390c7e21b0
Eugene Kvasnyi <e.kvasnyi@gmail.com>
2021-05-16 12:55:03 +0300

    Added: paddedString variable

--- _.js
+++ _.js
@@ -49,19 +49,18 @@ const _ = {
         //if string.length is equeal to length parameter, then return string arg without modification
         // .pad() adds spaces evenly to both sides of the string to make it reach the desired length
         // Extra padding is added to the end of the string if an odd amount of padding is required to reach the specified length
-        // Your method does not need to accept the additional chars parameter; we will only add space characters to pad our string
 
         if(string.length === length || string.length > length ){
             return string;
         }
-        
+
         const padLength = length-string.length
         const padStartSize = Math.floor(padLength / 2)
         const padEndSize = padLength - padStartSize;
 
-        return  ' '.repeat(padStartSize)
-                + string
-                + ' '.repeat(padEndSize);
+        const paddedString = ' '.repeat(padStartSize) + string + ' '.repeat(padEndSize);
+
+        return paddedString;
        
     }
 
